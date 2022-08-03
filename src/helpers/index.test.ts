786566dba07123cc6a91bfbc7d622c7694048671
Alex <alexmayolc@gmail.com>
2022-08-03 23:20:16 +0200

    feat: added crypto for reliable random indexes

--- src/helpers/index.test.ts
+++ src/helpers/index.test.ts
@@ -1,6 +1,15 @@
 import { emojis } from "../emojis";
 import { getRandomItemFrom } from "./index";
 
+const crypto = require('crypto');
+
+Object.defineProperty(global, 'crypto', {
+  value: {
+    getRandomValues: (arr: Uint32Array) => crypto.randomBytes(arr.length)
+  }
+});
+
+
 describe("Helpers test suites", () => {
   test("Can get a random emoji from the list", () => {
     const emo = getRandomItemFrom(emojis);
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,23 @@
+
+export const cryptoGetRandomIntBetween = (min: number, max: number) => {
+  const randomBuffer = new Uint8Array(1);
+
+  crypto.getRandomValues(randomBuffer);
+
+  let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(randomNumber * (max - min + 1)) + min;
+}
+
+
 const getRandomIntBetween = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min - 1) + min);
 
-export const getRandomItemFrom = <T>(items: T[]): T =>
-  items[getRandomIntBetween(0, items.length)];
+export const getRandomItemFrom = <T>(items: T[]): T => {
+  if (crypto)
+    return items[cryptoGetRandomIntBetween(0, items.length)];
+  return items[getRandomIntBetween(0, items.length)];
+}
+
