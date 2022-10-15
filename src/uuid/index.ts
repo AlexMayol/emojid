79ac008e1f3964296d2f9678e5249a97f98aad79
Alex <alexmayolc@gmail.com>
2022-10-15 22:45:51 +0200

    more emojis

--- src/uuid/index.ts
+++ src/uuid/index.ts
@@ -1,5 +1,5 @@
 import { getRandomItemFrom as get } from "../helpers";
 import { emojis } from "../emojis";
 
-export const uuid = () =>
+export const uuid = (): string =>
   "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx".replace(/xx/g, () => get(emojis));
